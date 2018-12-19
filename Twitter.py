81248c6b4179e2eba2c7c9e88dc50e035b5071d1
陈介青 <chenjieqing@aos-MacBook-puro.local>
2018-12-19 15:16:52 -0500

    cleancode

--- Twitter.py
+++ Twitter.py
@@ -26,20 +26,25 @@ def init_twitter(client_key, client_secret):
     client_secret = client_secret
 
     key_secret = '{}:{}'.format(client_key, client_secret).encode('ascii')
-    b64_encoded_key = base64.b64encode(key_secret)
-    b64_encoded_key = b64_encoded_key.decode('ascii')
-    base_url = 'https://api.twitter.com/'
-    auth_url = '{}oauth2/token'.format(base_url)
-    auth_headers = {
-        'Authorization': 'Basic {}'.format(b64_encoded_key),
-        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
-    }
-    auth_data = {
-        'grant_type': 'client_credentials'
-    }
-    auth_resp = requests.post(auth_url, headers=auth_headers, data=auth_data)
-    access_token = auth_resp.json()['access_token']
-    return None
+
+    return key_secret
+
+
+key_secret = init_twitter(client_key, client_secret)
+
+b64_encoded_key = base64.b64encode(key_secret)
+b64_encoded_key = b64_encoded_key.decode('ascii')
+base_url = 'https://api.twitter.com/'
+auth_url = '{}oauth2/token'.format(base_url)
+auth_headers = {
+    'Authorization': 'Basic {}'.format(b64_encoded_key),
+    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+}
+auth_data = {
+    'grant_type': 'client_credentials'
+}
+auth_resp = requests.post(auth_url, headers=auth_headers, data=auth_data)
+access_token = auth_resp.json()['access_token']
 
 
 class Media:
@@ -73,6 +78,7 @@ class Emoji:
 
 
 def search_twitter(name):
+
     search_headers = {'Authorization': 'Bearer {}'.format(access_token)}
     pd = {}
     pd["query"] = name
@@ -88,7 +94,6 @@ def search_twitter(name):
             Twitter_cache.set(unique_url, html_text, 30)
 
     return Twitter_cache.get(url)
-
 
 
 def get_emoji(twitter_data):
